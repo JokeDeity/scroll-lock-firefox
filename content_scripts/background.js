a8ddb2f6bb2a7f795687980b05e7afdd82d526df
JokeDeity <JokeDeity@users.noreply.github.com>
2025-03-25 23:53:22 -0400

    Update background.js

--- content_scripts/background.js
+++ content_scripts/background.js
@@ -1,9 +1,22 @@
 let isLocked = false;
 
-browser.browserAction.onClicked.addListener((tab) => {
-  isLocked = !isLocked; // Toggle state
-
+function toggleScrollLock(tab) {
+  isLocked = !isLocked;
   browser.tabs.sendMessage(tab.id, {
-    command: isLocked ? 'lock' : 'reset'
+    command: isLocked ? "lock" : "reset"
   });
+}
+
+browser.browserAction.onClicked.addListener((tab) => {
+  toggleScrollLock(tab);
+});
+
+browser.commands.onCommand.addListener((command) => {
+  if (command === "toggle-scroll-lock") {
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+      if (tabs.length > 0) {
+        toggleScrollLock(tabs[0]);
+      }
+    });
+  }
 });
